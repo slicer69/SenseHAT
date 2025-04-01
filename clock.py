efe8466671fb6fdb3f5c5084a38c2bb028377a02
Jesse <jsmith@resonatingmedia.com>
2025-04-01 19:31:36 -0300

    Added initial Clock code and latest draft of Nibbles.

---
+++ clock.py
@@ -0,0 +1,143 @@
+# from sense_hat import SenseHat
+import math
+import time
+import sys
+import signal
+
+
+SLEEP_DELAY = 0.5
+
+BRIGHTNESS = 150
+RED = [BRIGHTNESS,0,0]
+GREEN = [0,BRIGHTNESS,0]
+BLUE = [0,0,BRIGHTNESS]
+BLACK = [0,0,0]
+B = BLACK
+
+canvas = [
+B, B, B, B, B, B, B, B,
+B, B, B, B, B, B, B, B,
+B, B, B, B, B, B, B, B,
+B, B, B, B, B, B, B, B,
+B, B, B, B, B, B, B, B,
+B, B, B, B, B, B, B, B,
+B, B, B, B, B, B, B, B,
+B, B, B, B, B, B, B, B,
+]
+
+CANVAS_WIDTH = 8
+CANVAS_HEIGHT = 8
+NUMBER_HEIGHT = 4
+NUMBER_WIDTH = 4
+NUMBER_SIZE = NUMBER_HEIGHT * NUMBER_WIDTH
+
+numbers = [
+0,1,1,1, # Zero
+0,1,0,1,
+0,1,0,1,
+0,1,1,1,
+0,0,1,0, # One
+0,1,1,0,
+0,0,1,0,
+0,1,1,1,
+0,1,1,1, # Two
+0,0,1,1,
+0,1,1,0,
+0,1,1,1,
+0,1,1,1, # Three
+0,0,1,1,
+0,0,1,1,
+0,1,1,1,
+0,1,0,1, # Four
+0,1,1,1,
+0,0,0,1,
+0,0,0,1,
+0,1,1,1, # Five
+0,1,1,0,
+0,0,1,1,
+0,1,1,1,
+0,1,0,0, # Six
+0,1,1,1,
+0,1,0,1,
+0,1,1,1,
+0,1,1,1, # Seven
+0,0,0,1,
+0,0,1,0,
+0,1,0,0,
+0,1,1,1, # Eight
+0,1,1,1,
+0,1,1,1,
+0,1,1,1,
+0,1,1,1, # Nine
+0,1,0,1,
+0,1,1,1,
+0,0,0,1
+]
+
+# sense = SenseHat()
+
+def signal_handler(my_signal, temp):
+   # sense.clear()
+   sys.exit(0)
+
+
+def Clear_Canvas():
+   for index in range(0, CANVAS_WIDTH * CANVAS_HEIGHT):
+      canvas[index] = BLACK
+
+
+def Place_Number_On_Canvas(x, y, value, colour):
+   if value > 9:
+       tens = math.trunc(value / 10)
+       ones = value % 10
+   else:
+       tens = 0
+       ones = value
+
+   canvas_index = y * CANVAS_WIDTH + x
+   number_index = tens * NUMBER_SIZE
+   for y in range(0, NUMBER_HEIGHT):
+     for x in range(0, NUMBER_WIDTH):
+         if numbers[number_index] == 1:
+            canvas[canvas_index] = colour
+         else:
+            canvas[canvas_index] = BLACK
+         canvas_index += 1
+         number_index += 1
+     canvas_index = canvas_index + (CANVAS_WIDTH - NUMBER_WIDTH - 1)
+
+   canvas_index = y * CANVAS_WIDTH + x + NUMBER_WIDTH
+   number_index = ones * NUMBER_SIZE
+   for y in range(0, NUMBER_HEIGHT):
+     for x in range(0, NUMBER_WIDTH):
+         if numbers[number_index] == 1:
+            canvas[canvas_index] = colour
+         else:
+            canvas[canvas_index] = BLACK
+         canvas_index += 1
+         number_index += 1
+     canvas_index = canvas_index + (CANVAS_WIDTH - NUMBER_WIDTH - 1)
+
+
+def main():
+   signal.signal(signal.SIGINT, signal_handler)
+   # sense.set_rotation(180)
+
+   my_time = time.localtime()
+   my_minute = my_time.tm_min
+   my_hour = my_time.tm_hour
+
+   print(my_hour, ":", my_minute)
+   Place_Number_On_Canvas(0, 0, my_hour, GREEN)
+   Place_Number_On_Canvas(0, 4, my_minute, RED)
+   print(canvas)
+   # sense.clear()
+   # sense.set_pixels(canvas)
+   Clear_Canvas()
+   time.sleep(SLEEP_DELAY)
+
+
+
+if __name__ == "__main__":
+    main()
+
--- nibbles.py
+++ nibbles.py
@@ -1,4 +1,5 @@
 from sense_hat import SenseHat
+import math
 import signal
 import sys
 import time
@@ -23,6 +24,11 @@ BOARD_HEIGHT = 8
 BOARD_SIZE = BOARD_WIDTH * BOARD_HEIGHT
 
 REFRESH_DELAY = 1.0
+
+DIR_UP = 0
+DIR_DOWN = 2
+DIR_LEFT = 1
+DIR_RIGHT = 3
 
 
 def signal_handler(my_signal, temp):
@@ -56,9 +62,9 @@ def Place_Snake_On_Board(my_board, snake, max_snake):
    snake_index = 0
    while snake_index < len(snake):
        x_y = snake[snake_index]
-       board_index = x_y[1] * BOARD_WIDTH + x_y[0]
+       board_index = math.trunc( x_y[1] * BOARD_WIDTH + x_y[0] )
        my_board[board_index] = SNAKE_COLOUR
-
+       snake_index += 1
 
 
 # Detect if we ran into ourselves or the side of the wall.
@@ -72,12 +78,59 @@ def Detect_Collision(the_board, x, y):
        return True
 
     # See if we ran into our own colour/tail
-    offset = (y * BOARD_WIDTH) + x
+    offset = math.trunc( (y * BOARD_WIDTH) + x )
     if the_board[offset] == SNAKE_COLOUR:
         return True
 
     return False
 
+
+# Get an action from the joystick. Waits for the joystick to
+# be released before returning the last direction/action
+# given.
+# When no input is available we return False.
+def Joystick_Action():
+  # Check for any joystick events
+  all_events = sense.stick.get_events()
+  while len(all_events) > 0:
+      this_event = all_events.pop(0)
+      if this_event.action == "released":
+          return this_event.direction
+          
+  return False
+
+
+
+
+# Check for input from joystick.
+# Perform validation to make sure the plyer
+# is not trying to reverse course (180 degrees).
+# Return the new direction.
+def Get_Player_Move(old_direction):
+    # In the case of no good input, keep the old direction
+    new_direction = old_direction
+
+    direction = Joystick_Action()
+    # remember, direction of stick is inverse
+    if direction == "up" and old_direction != DIR_UP:
+        new_direction = DIR_DOWN
+    elif direction == "down" and old_direction != DIR_DOWN:
+        new_direction = DIR_UP
+    elif direction == "left" and old_direction != DIR_LEFT:
+        new_direction = DIR_RIGHT
+    elif direction == "right" and old_direction != DIR_RIGHT:
+        new_direction = DIR_LEFT
+    elif direction == "middle":
+        # Pause. Loop until middle button is pressed again.
+        pause = True
+        while pause:
+           direction = Joystck_Action()
+           if direction == "middle":
+              pause = False
+           else:
+              time.sleep(REFRESH_DELAY)
+
+    return new_direction
 
 
 def main():
@@ -96,17 +149,21 @@ def main():
    signal.signal(signal.SIGINT, signal_handler)
     
    # Pick starting position and direction
-   x = BOARD_WIDTH / 2
-   y = BOARD_HEIGHT / 2
+   x = math.trunc( BOARD_WIDTH / 2 )
+   y = math.trunc( BOARD_HEIGHT / 2 )
    max_snake_length = 2
    x_y_pair = (x, y)
    snake_position.append(x_y_pair)
-   delta_x = random.randint(-1, 1)
-   delta_y = random.randint(-1, 1)
-   # Do not allow the snake to start motionless
-   if delta_x == 0 and delta_y == 0:
-      delta_x = 1
+   direction = random.randint(0, 3)
+   if direction == DIR_UP or direction == DIR_DOWN:
+      delta_x = 0
+      delta_y = direction - 1
+   elif direction == DIR_LEFT or direction == DIR_RIGHT:
+      delta_y = 0
+      delta_x = direction - 2
 
+
+   # This is the main game loop
    while not finished:
       # add snake to board
       Place_Snake_On_Board(my_board, snake_position, max_snake_length)
@@ -116,11 +173,21 @@ def main():
 
       # Check for input to change direction or pause
       # Make sure we are not turning back on ourselves
+      direction = Get_Player_Move(direction)
+      if direction == DIR_UP or direction == DIR_DOWN:
+          delta_x = 0
+          delta_y = direction - 1
+      elif direction == DIR_LEFT or direction == DIR_RIGHT:
+          delta_y = 0
+          delta_x = direction - 2
+
       # Adjust delta_x and delta_y
-     
       # update snake position, change x and y
       x += delta_x
       y += delta_y
+      # Add new position to the head of the snake
+      x_y_pair = (x, y)
+      snake_position.append(x_y_pair)
 
       # Detect if we ran into ourselves or a wall
       collision = Detect_Collision(my_board, x, y)
